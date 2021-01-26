4088eb5c1403719075e402e11460365c9c072535
david11yf29 <david11yf29@gmail.com>
2021-01-26 20:33:30 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import SummaryForm from "./pages/summary/SummaryForm.jsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SummaryForm />
     </div>
   );
 }
